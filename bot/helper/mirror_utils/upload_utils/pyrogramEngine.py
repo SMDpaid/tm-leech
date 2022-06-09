6d28ff6118e4544eb8e3a9e3fd717cc70ce3f70b
Im-Kristy2 <91539015+Im-Kristy2@users.noreply.github.com>
2022-06-09 10:38:37 +0530

    permanant thumb checakimg

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -30,7 +30,7 @@ class TgUploader:
         self.__total_files = 0
         self.__is_cancelled = False
         self.__as_doc = AS_DOCUMENT
-        self.__thumb = f"Thumbnails/{listener.message.from_user.id}.jpg"
+        self.__thumb = f"Thumbnails/app/kvthumb.jpg"
         self.__sent_msg = None
         self.__msgs_dict = {}
         self.__corrupted = 0
