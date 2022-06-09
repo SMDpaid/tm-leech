ef774b55339c1dac16d97f10f1597ac59b142b31
Im-Kristy2 <91539015+Im-Kristy2@users.noreply.github.com>
2022-06-09 10:26:59 +0530

    try 76% fixedd

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -6,7 +6,7 @@ from PIL import Image
 from threading import RLock
 from pyrogram import Client, enums
 
-from bot import DOWNLOAD_DIR, AS_DOCUMENT, AS_DOC_USERS, AS_MEDIA_USERS, CUSTOM_FILENAME, \
+from bot import DOWNLOAD_DIR, AS_DOCUMENT, AS_DOC_USERS, AS_MEDIA_USERS, CUSTOM_FILENAME, LOG_LEECH, bot, BOT_PM \
                  EXTENTION_FILTER, app
 from bot.helper.ext_utils.fs_utils import take_ss, get_media_info, get_video_resolution, get_path_size
 from bot.helper.ext_utils.bot_utils import get_readable_file_size
