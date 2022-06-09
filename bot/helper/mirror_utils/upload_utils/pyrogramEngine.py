d263e48890be011eacc758c8a5a626723a4dc539
Im-Kristy2 <91539015+Im-Kristy2@users.noreply.github.com>
2022-06-09 10:14:17 +0530

    fuckk erros

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -22,46 +22,46 @@ class TgUploader:
 
     def __init__(self, name=None, listener=None):
         self.name = name
-        self.__app = app
         self.uploaded_bytes = 0
         self._last_uploaded = 0
         self.__listener = listener
         self.__start_time = time()
+        self.__total_files = 0
         self.__is_cancelled = False
         self.__as_doc = AS_DOCUMENT
-        self.__thumb = f"Thumbnails/app/kvthumb.jpg"
-        self.__sent_msg = ''
+        self.__thumb = f"Thumbnails/{listener.message.from_user.id}.jpg"
+        self.__sent_msg = None
         self.__msgs_dict = {}
         self.__corrupted = 0
         self.__resource_lock = RLock()
+        self.__is_corrupted = False
+        self.__sent_msg = app.get_messages(self.__listener.message.chat.id, self.__listener.uid)
         self.__user_settings()
 
     def upload(self):
         path = f"{DOWNLOAD_DIR}{self.__listener.uid}"
-        size = get_path_size(path)
+        size = get_readable_file_size(get_path_size(path))
         for dirpath, subdir, files in sorted(walk(path)):
             for file_ in sorted(files):
-                if self.__is_cancelled:
-                    return
-                if file_.endswith('.torrent'):
-                    continue
-                up_path = ospath.join(dirpath, file_)
-                fsize = ospath.getsize(up_path)
-                if fsize == 0:
-                    LOGGER(__name__).error(f"{up_path} size is zero, telegram don't upload zero size files")
-                    self.__corrupted += 1
-                    continue
-                self.__upload_file(up_path, file_, dirpath)
-                if self.__is_cancelled:
-                    return
-                if not self.__listener.isPrivate and not self.__is_corrupted:
+                if not file_.lower().endswith(tuple(EXTENTION_FILTER)):
+                    self.__total_files += 1
+                    up_path = ospath.join(dirpath, file_)
+                    if ospath.getsize(up_path) == 0:
+                        LOGGER.error(f"{up_path} size is zero, telegram don't upload zero size files")
+                        self.__corrupted += 1
+                        continue
+                    self.__upload_file(up_path, file_, dirpath)
+                    if self.__is_cancelled:
+                        return
+                    if not self.__listener.isPrivate and not self.__is_corrupted:
                         self.__msgs_dict[self.__sent_msg.link] = file_
-                self._last_uploaded = 0
-                sleep(1)
-        if len(self.__msgs_dict) <= self.__corrupted:
+                    self._last_uploaded = 0
+                    sleep(1)
+        if self.__total_files <= self.__corrupted:
             return self.__listener.onUploadError('Files Corrupted. Check logs')
-        LOGGER(__name__).info(f"Leech Completed: {self.name}")
-        self.__listener.onUploadComplete(self.name, size, self.__msgs_dict, None, self.__corrupted)
+        LOGGER.info(f"Leech Completed: {self.name}")
+        self.__listener.onUploadComplete(None, size, self.__msgs_dict, self.__total_files, self.__corrupted, self.name)
+
 
     def __upload_file(self, up_path, file_, dirpath):
         if self.__sent_msg == '':
