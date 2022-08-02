0f1a63468b651397f56d447589e6038b4f4590ca
Thevdiya <106153043+Thevdiya@users.noreply.github.com>
2022-08-02 21:05:32 +0530

    Try

--- bot/__init__.py
+++ bot/__init__.py
@@ -25,7 +25,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
                     level=logging.INFO)
 
-LOGGER = logging.getLogger()
+LOGGER = logging.getLogger(__name__)
 
 load_dotenv('config.env', override=True)
 
