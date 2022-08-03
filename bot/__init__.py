790a0ffd58073042b55ead38b5039b3a342bd587
Thevdiya <106153043+Thevdiya@users.noreply.github.com>
2022-08-03 17:13:22 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -21,7 +21,7 @@ socket.setdefaulttimeout(600)
 
 botStartTime = time()
 
-basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
                     level=logging.INFO)
 
